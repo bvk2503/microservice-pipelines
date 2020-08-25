7d16904831252ad4a278f64e58b125ccb2c040c0
bvk2503 <34239494+bvk2503@users.noreply.github.com>
2020-08-26 01:51:35 +0530

    Update jenkinsJob.groovy

--- vars/jenkinsJob.groovy
+++ vars/jenkinsJob.groovy
@@ -1,3 +1,10 @@
+#!/usr/bin/env groovy
+
+// Configure using microservice-pipelines and using "part2" branch
+@Library("microservice-pipelines@part2") _
+
+// Entry point into microservice-pipelines
+jenkinsJob.call()
 def call(){
     node {
         stage('Checkout') {
